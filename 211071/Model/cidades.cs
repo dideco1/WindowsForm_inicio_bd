c0b56b78873cecd0f9a45811cbf9f36bfeec31db
dideco1 <diegobaro.si@outlook.com>
2022-10-05 11:26:53 -0300

    Metodo alterar (update) na tabela cidades

--- 211071/Model/cidades.cs
+++ 211071/Model/cidades.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using System.Data;
+using System.Windows.Forms;
 
 namespace _211071.Model
 {
@@ -16,15 +17,38 @@ namespace _211071.Model
 
         public void incluir()
         {
-            Banco.AbrirConexao();
-            // Da o comando desejado e logo em seguida qual conexão será utilizada para executar a instrução
-            Banco.comando = new MySqlCommand("INSERT INTO cidades(nome,uf) VALUES (@nome, @uf)", Banco.conexao);
-            // Da o valor dos parâmetros ditos no comando acima
-            Banco.comando.Parameters.AddWithValue("@nome", nome);
-            Banco.comando.Parameters.AddWithValue("@uf", uf);
+            try
+            {
+                Banco.AbrirConexao();
+                // Da o comando desejado e logo em seguida qual conexão será utilizada para executar a instrução
+                Banco.comando = new MySqlCommand("INSERT INTO cidades(nome,uf) VALUES (@nome, @uf)", Banco.conexao);
+                // Da o valor dos parâmetros ditos no comando acima
+                Banco.comando.Parameters.AddWithValue("@nome", nome);
+                Banco.comando.Parameters.AddWithValue("@uf", uf);
 
-            Banco.comando.ExecuteNonQuery();
-            Banco.FecharConexao();
+                Banco.comando.ExecuteNonQuery();
+                Banco.FecharConexao();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public void alterar()
+        {
+            try
+            {
+                Banco.AbrirConexao();
+                Banco.comando = new MySqlCommand("UPDATE cidades SET nome = @nome, uf = @uf WHERE id = @id", Banco.conexao);
+
+                Banco.comando.Parameters.AddWithValue("@nome", nome);
+                Banco.comando.Parameters.AddWithValue("@uf", uf);
+                Banco.comando.Parameters.AddWithValue("@id", id);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
