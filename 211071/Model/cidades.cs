68656fdffbd138489023c28f95e1d602c8b47c3e
dideco1 <diegobaro.si@outlook.com>
2022-10-05 11:10:17 -0300

    Metodo "INSERT INTO" na tabela cidades

--- 211071/Model/cidades.cs
+++ 211071/Model/cidades.cs
@@ -13,5 +13,18 @@ namespace _211071.Model
         int id { get; set; }
         string nome { get; set; }
         int uf { get; set; }
+
+        public void incluir()
+        {
+            Banco.AbrirConexao();
+            // Da o comando desejado e logo em seguida qual conexão será utilizada para executar a instrução
+            Banco.comando = new MySqlCommand("INSERT INTO cidades(nome,uf) VALUES (@nome, @uf)", Banco.conexao);
+            // Da o valor dos parâmetros ditos no comando acima
+            Banco.comando.Parameters.AddWithValue("@nome", nome);
+            Banco.comando.Parameters.AddWithValue("@uf", uf);
+
+            Banco.comando.ExecuteNonQuery();
+            Banco.FecharConexao();
+        }
     }
 }
