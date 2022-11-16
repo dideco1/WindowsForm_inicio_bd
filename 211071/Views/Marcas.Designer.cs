ad3df7f546fbd9a48dfa4ad7c3db2adad3aedbf9
dideco1 <diegobaro.si@outlook.com>
2022-11-16 11:53:31 -0300

    Add marcas

---
+++ 211071/Views/Marcas.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace _211071.Views
+{
+    partial class Marcas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv_marcas = new System.Windows.Forms.DataGridView();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.txt_pesquisa = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnIncluir = new System.Windows.Forms.Button();
+            this.txt_marca = new System.Windows.Forms.TextBox();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_fechar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_marcas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv_marcas
+            // 
+            this.dgv_marcas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_marcas.Location = new System.Drawing.Point(11, 266);
+            this.dgv_marcas.Name = "dgv_marcas";
+            this.dgv_marcas.Size = new System.Drawing.Size(644, 150);
+            this.dgv_marcas.TabIndex = 42;
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Location = new System.Drawing.Point(562, 208);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(94, 52);
+            this.btnPesquisar.TabIndex = 41;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // txt_pesquisa
+            // 
+            this.txt_pesquisa.Location = new System.Drawing.Point(12, 230);
+            this.txt_pesquisa.Name = "txt_pesquisa";
+            this.txt_pesquisa.Size = new System.Drawing.Size(544, 20);
+            this.txt_pesquisa.TabIndex = 40;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(8, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(286, 20);
+            this.label4.TabIndex = 39;
+            this.label4.Text = "Digite o nome da Marca para Pesquisa:";
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(392, 130);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(106, 66);
+            this.btnExcluir.TabIndex = 37;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(275, 131);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(102, 65);
+            this.btnCancelar.TabIndex = 36;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(143, 130);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(112, 66);
+            this.btnAlterar.TabIndex = 35;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnIncluir
+            // 
+            this.btnIncluir.Location = new System.Drawing.Point(11, 131);
+            this.btnIncluir.Name = "btnIncluir";
+            this.btnIncluir.Size = new System.Drawing.Size(113, 65);
+            this.btnIncluir.TabIndex = 34;
+            this.btnIncluir.Text = "Incluir";
+            this.btnIncluir.UseVisualStyleBackColor = true;
+            this.btnIncluir.Click += new System.EventHandler(this.btnIncluir_Click);
+            // 
+            // txt_marca
+            // 
+            this.txt_marca.Location = new System.Drawing.Point(12, 93);
+            this.txt_marca.Name = "txt_marca";
+            this.txt_marca.Size = new System.Drawing.Size(486, 20);
+            this.txt_marca.TabIndex = 33;
+            // 
+            // txt_id
+            // 
+            this.txt_id.Location = new System.Drawing.Point(12, 34);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(100, 20);
+            this.txt_id.TabIndex = 32;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "Marca:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 20);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "Id:";
+            // 
+            // btn_fechar
+            // 
+            this.btn_fechar.Location = new System.Drawing.Point(516, 131);
+            this.btn_fechar.Name = "btn_fechar";
+            this.btn_fechar.Size = new System.Drawing.Size(106, 66);
+            this.btn_fechar.TabIndex = 43;
+            this.btn_fechar.Text = "Fechar";
+            this.btn_fechar.UseVisualStyleBackColor = true;
+            this.btn_fechar.Click += new System.EventHandler(this.btn_fechar_Click);
+            // 
+            // Marcas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 450);
+            this.Controls.Add(this.btn_fechar);
+            this.Controls.Add(this.dgv_marcas);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.txt_pesquisa);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnIncluir);
+            this.Controls.Add(this.txt_marca);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Marcas";
+            this.Text = "Marcas";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_marcas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv_marcas;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.TextBox txt_pesquisa;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnIncluir;
+        private System.Windows.Forms.TextBox txt_marca;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_fechar;
+    }
+}
---
+++ 211071/Views/Marcas.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using _211071.Model;
+
+namespace _211071.Views
+{
+    public partial class Marcas : Form
+    {
+        marcas m;
+        public Marcas()
+        {
+            InitializeComponent();
+        }
+        void LimparControles()
+        {
+            txt_id.Clear();
+            txt_marca.Clear();
+            txt_pesquisa.Clear();
+        }
+        void CarregarGrid(String pesquisa)
+        {
+            m = new marcas()
+            {
+                marca = pesquisa
+            };
+            dgv_marcas.DataSource = m.Consultar();
+        }
+
+        private void btn_fechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            CarregarGrid(txt_pesquisa.Text);
+        }
+
+        private void btnIncluir_Click(object sender, EventArgs e)
+        {
+            if (txt_marca.Text == String.Empty) return;
+
+            m = new marcas()
+            {
+                marca = txt_marca.Text,
+            };
+
+            m.Incluir();
+
+            LimparControles();
+            CarregarGrid("");
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            if (txt_id.Text == String.Empty) return;
+
+            m = new marcas()
+            {
+                id = int.Parse(txt_id.Text),
+                marca = txt_marca.Text,
+            };
+            m.Alterar();
+
+            LimparControles();
+            CarregarGrid("");
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            LimparControles();
+            CarregarGrid("");
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (txt_id.Text == "") return;
+
+            if (MessageBox.Show("Deseja excluir a marca?", "Exclusão",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                m = new marcas()
+                {
+                    id = int.Parse(txt_id.Text)
+                };
+                m.Excluir();
+
+                LimparControles();
+                CarregarGrid("");
+            }
+        }
+    }
+}
